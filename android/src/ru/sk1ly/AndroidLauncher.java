b95caf2d3855dd45824fc6250b72ece8ec8182e1
sk1ly <io.skvortsov@mail.ru>
2021-02-25 18:09:57 +0300

    Инициализация проекта

---
+++ android/src/ru/sk1ly/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.sk1ly;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.sk1ly.RaindropGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RaindropGame(), config);
+	}
+}
